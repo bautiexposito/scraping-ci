a5b808bd8cc08a6a193c10bd14a1309f3b2ae23b
bautiexposito <bautistaexposito@outlook.com>
2025-02-20 19:29:38 -0300

    sql queries

---
+++ scraping_db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE scraping_db;
+USE scraping_db;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    phone VARCHAR(20) NOT NULL
+);
